91b785340dadb9a4a3c9304f2576f32cacf4c70b
seojaemin <dydgns11@naver.com>
2019-04-16 20:50:46 +0900

    jjjjj

---
+++ 4People_last2Spring/src/com/chatting/test.java
@@ -0,0 +1,12 @@
+package com.chatting;
+
+public class test {
+
+	public static void main(String[] args) {
+		String str = "new2";
+		String str2[] = str.split("w");
+		System.out.println(str2[1]);
+
+	}
+
+}
